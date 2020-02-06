d1ca52fcc1b7e30346281d9c3f37bd7771f9fa53
IAleks94 <a12344@mail.ru>
2020-02-06 15:48:54 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 let mySwiper = new Swiper(".swiper-container", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: ".arrow",
   },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    }
+}
 });
+
+let menuButton = document.querySelector('.menu-btn');
+let menu = document.querySelector('.header');    
+let openMenu = evt => {
+  menu.classList.toggle('hader-active');
+  menuButton.classList.toggle('menu-btn-active')
+}
+
+
+
+menuButton.addEventListener('click', openMenu);
